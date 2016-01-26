5a6b875c3ea750ffa0c0322db4379f06af2f21ad
Diti Torterat <kra@diti.me>
2016-01-26 15:04:54 +0100

    Add support for zsh-syntax-highlighting

--- .zsh/zsh_fpath.zsh
+++ .zsh/zsh_fpath.zsh
@@ -1,5 +1,10 @@
 if command_exists 'brew'; then
-	if brew list -1 | grep -q "^zsh-completions\$"; then
-		fpath=(${BREW_PREFIX}/share/zsh-completions $fpath)
-	fi
+    if brew list -1 | grep -q "^zsh-completions\$"; then
+        fpath=(${BREW_PREFIX}/share/zsh-completions $fpath)
+    fi
+
+    local hlfile="${BREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+    if brew list -1 | grep -q "^zsh-syntax-highlighting\$" && [ -f "$hlfile" ]; then
+        source "$hlfile"
+    fi
 fi
