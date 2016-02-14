f33a2129f5dc85856a958e928e320560acefe6ab
Diti Torterat <kra@diti.me>
2016-02-14 14:46:09 +0100

    Fix “brew not found” message

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -3,12 +3,12 @@ set -o ALL_EXPORT
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 
+command_exists 'brew' && BREW_PREFIX=$(brew --prefix)
+
 if command_exists 'vim'; then
 	EDITOR=vim
 	USE_EDITOR=$EDITOR
 	VISUAL=$EDITOR
 fi
-
-BREW_PREFIX=$(brew --prefix)
 
 set +o ALL_EXPORT
