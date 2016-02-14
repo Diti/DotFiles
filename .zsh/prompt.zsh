de607c698e0a7a3281108a7320d20575fb03a7a9
Diti Torterat <kra@diti.me>
2016-02-14 14:57:58 +0100

    root prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,2 +1,6 @@
-PROMPT="%F{green}%n%f@%F{blue}%m%f ❯ " # `diti@Iceberg ❯ `
+if [ $USER = root ]; then
+    PROMPT="%F{red}%n%f@%F{blue}%m%f ! "
+else
+    PROMPT="%F{green}%n%f@%F{blue}%m%f ❯ " # `diti@Iceberg ❯ `
+fi
 RPROMPT="%~"                           # `~/proj`
