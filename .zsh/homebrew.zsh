c426ebdfd193494d7609447c304db6fd600ed20b
Diti Torterat <kra@diti.me>
2016-03-08 16:44:59 +0100

    More generic Homebrew install if not root

--- .zsh/homebrew.zsh
+++ .zsh/homebrew.zsh
@@ -1,23 +1,25 @@
-if command_exists 'brew' && [[ "$HOST" =~ ".*42.fr$" ]]; then
+if command_exists 'brew'; then
 
-    export BREW_PREFIX="$HOME/.brew"
+    # If we don’t have root rights, use our own Homebrew install on $HOME
+    if [ ! -w "/Library/Caches/Homebrew" ]; then
 
-    export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications --binarydir=${BREW_PREFIX}/bin --caskroom=${BREW_PREFIX}/Caskroom"
+        BREW_PREFIX="$HOME/.brew"
 
-    # If not using our local brew, init the school’s one and reload the path
-    if [ ! -d "$BREW_PREFIX" ]; then
-        test -f /usr/local/bin/brew && $_ update && source ~/.zshrc
+        export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications --binarydir=${BREW_PREFIX}/bin --caskroom=${BREW_PREFIX}/Caskroom"
+
+        # Setup our own local Homebrew install if needed
+        if [ ! -d "$BREW_PREFIX" ]; then
+            test -f /usr/local/bin/brew && $_ update && source ~/.zshrc
+        fi
+
+        mkdir -p "$HOME/Library/Caches/Homebrew" && export HOMEBREW_CACHE=$_
+
     fi
 
-    # Use our own directories for Homebrew data
-    local tmpDataDir="/tmp/${USER}-brew-$(date '+%Y%m%d')"
+    # Ensure $BREW_PREFIX is set
+    : ${BREW_PREFIX:=$(brew --prefix)}
 
-    mkdir -p ${tmpDataDir}/{cache,tmp}
-
-    test -d "${tmpDataDir}/cache" && export HOMEBREW_CACHE=$_
-    test -d "${tmpDataDir}/tmp" && export HOMEBREW_TEMP=$_
+    # Use Homebrew’s binaries instead of the system’s
+    export PATH=${BREW_PREFIX}/sbin:${BREW_PREFIX}/bin:${PATH}
 
 fi
-
-# Use Homebrew’s binaries instead of the system’s
-export PATH=${BREW_PREFIX}/sbin:${BREW_PREFIX}/bin:${PATH}
