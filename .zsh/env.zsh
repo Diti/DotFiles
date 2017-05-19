568b546e92cf50e6f2b82de90d33c790f716d6da
Dimitri Torterat <kra@diti.me>
2017-05-19 15:49:00 +0200

    GnuPG options

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -5,7 +5,15 @@ export PATH=$HOME/.composer/vendor/bin:$PATH
 
 command_exists 'brew' && export BREW_PREFIX=$(brew --prefix)
 command_exists 'go' && export PATH=$(go env GOPATH)/bin:$PATH
-command_exists 'gpg' || command_exists 'gpg2' && export GPG_TTY=$(tty)
+
+if command_exists 'gpg' || command_exists 'gpg2'; then
+    export GPG_TTY=$(tty)
+    eval $(gpg-agent --enable-ssh-support --daemon 2>/dev/null)
+    gpg-connect-agent updatestartuptty /bye >/dev/null
+    unset SSH_AGENT_PID
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+
 command_exists 'ocaml' && export OCAMLPARAM='w=A,_'
 
 if command_exists 'vim'; then
